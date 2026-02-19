031ee251fdea699b3ca87763ba0d405c47c5c4c4
Radhe Patel <radhepatel485@example.com>
2026-02-19 21:43:53 +0530

    Practical 4 Java programs added

---
+++ Practical_4/AlphabetFullPyramid.java
@@ -0,0 +1,20 @@
+public class AlphabetFullPyramid {
+    public static void main(String[] args) {
+
+        char[] letters = {'A','B','C','D','E'};
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int space = 0; space < i; space++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print(letters[j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
